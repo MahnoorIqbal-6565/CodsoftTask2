317ce26057fb9ee26ab9d83fe3e64b1059869d49
Mahnoor Iqbal <mahnooriqbal6565@gmail.com>
2023-09-12 11:01:07 -0700

    html landing page- gym website

---
+++ script.js
@@ -0,0 +1,48 @@
+// script.js
+const slider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+
+function prevSlide() {
+    if (currentIndex === 0) {
+        currentIndex = slider.children.length - 1;
+    } else {
+        currentIndex--;
+    }
+    updateSlider();
+}
+
+function nextSlide() {
+    if (currentIndex === slider.children.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+    updateSlider();
+}
+
+function updateSlider() {
+    const translateX = -currentIndex * 100;
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+
+document.getElementById("filter").addEventListener("change", filterImages);
+
+function filterImages() {
+    const category = document.getElementById("filter").value;
+    const images = document.querySelectorAll(".image");
+
+    images.forEach(image => {
+        if (category === "all" || image.classList.contains(category)) {
+            image.style.display = "block";
+        } else {
+            image.style.display = "none";
+        }
+    });
+}
